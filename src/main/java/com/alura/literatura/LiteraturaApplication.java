f068e0efee7799b78e0c9b37b1566c44bfe5b8b7
Fabiano Del Villar <fabianodelvillar@gmail.com>
2024-06-14 11:32:06 -0500

    Modificamos LiteraturaApplication para poder usar la consola

--- src/main/java/com/alura/literatura/LiteraturaApplication.java
+++ src/main/java/com/alura/literatura/LiteraturaApplication.java
@@ -1,13 +1,30 @@
 package com.alura.literatura;
 
+import com.alura.literatura.principal.Principal;
+import com.alura.literatura.repository.AutorRepository;
+import com.alura.literatura.repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraturaApplication {
+public class LiteraturaApplication implements CommandLineRunner
+{
+	@Autowired
+	private LibroRepository libroRepository;
+	@Autowired
+	private AutorRepository autorRepository;
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(LiteraturaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception
+	{
+		Principal principal = new Principal(libroRepository, autorRepository);
+		principal.menu();
+	}
 }
